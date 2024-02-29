3b974a2bf911f5349b65c025b5f98eca0b371727
paulman.2 <paulman.2@cse-sl2.coeit.osu.edu>
2024-02-28 22:56:40 -0500

    finished prototypes

--- p3.c
+++ p3.c
@@ -1,1 +1,80 @@
 /* Elijah Paulman */
+
+/*
+    Prototype 3 tests sorting a linked list.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+struct Thing 
+{
+    char* str;
+    struct Thing *next;
+};
+
+typedef bool (* ComparisonFunction)(void *data1, void *data2);
+
+void sort(struct Thing **head, ComparisonFunction cf) 
+{
+    struct Thing *current, *index;
+    char *temp;
+
+    for(current = *head; current->next != NULL; current = current->next) 
+    {
+        for(index = current->next; index != NULL; index = index->next) 
+        {
+            if(cf(current->str, index->str))
+             {
+                temp = current->str;
+                current->str = index->str;
+                index->str = temp;
+            }
+        }
+    }
+}
+
+bool compareStrings(void *data1, void *data2) 
+{
+    char *str1 = (char *)data1;
+    char *str2 = (char *)data2;
+    return strcmp(str1, str2) > 0;
+}
+
+void printList(struct Thing* head) 
+{
+    struct Thing* temp = head;
+    while(temp != NULL) 
+    {
+        printf("%s -> ", temp->str);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+struct Thing* createNode(char* str) {
+    struct Thing* newNode = (struct Thing*)malloc(sizeof(struct Thing));
+    newNode->str = str;
+    newNode->next = NULL;
+    return newNode;
+}
+
+int main() 
+{
+    struct Thing* head = createNode("World");
+    head->next = createNode("Hello");
+    head->next->next = createNode("Test");
+    head->next->next->next = createNode("Systems");
+
+    printf("Before sorting:\n");
+    printList(head);
+
+    sort(&head, compareStrings);
+
+    printf("After sorting:\n");
+    printList(head);
+
+    return 0;
+}
--- p4.c
+++ p4.c
@@ -1,1 +1,56 @@
 /* Elijah Paulman */
+
+/*
+    Prototype 4 tests the llib any function
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+struct Thing {
+    char* str;
+    struct Thing *next;
+};
+
+typedef bool (* CriteriaFunction)(void *data, void *helper);
+
+bool any(struct Thing *head, CriteriaFunction yes, void *helper) {
+    struct Thing *current = head;
+    while (current != NULL) {
+        if (yes(current->str, helper)) {
+            return true;
+        }
+        current = current->next;
+    }
+    return false;
+}
+
+bool isMatch(void *data, void *helper) {
+    char *str = (char *)data;
+    char *toMatch = (char *)helper;
+    return strcmp(str, toMatch) == 0;
+}
+
+struct Thing* createNode(char* str) {
+    struct Thing* newNode = (struct Thing*)malloc(sizeof(struct Thing));
+    newNode->str = str;
+    newNode->next = NULL;
+    return newNode;
+}
+
+int main() {
+    struct Thing* head = createNode("Hello");
+    head->next = createNode("World");
+    head->next->next = createNode("Test");
+    head->next->next->next = createNode("Systems");
+
+    char* toMatch = "Systems";
+    if (any(head, isMatch, toMatch)) {
+        printf("Match found!\n");
+    } else {
+        printf("Match not found!\n");
+    }
+
+    return 0;
+}
